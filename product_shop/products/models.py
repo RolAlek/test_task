4391ae5a679fd962a3c8ddc62ab9002a30824ec1
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-07-20 13:54:48 +0900

    create and add Category and SubCategory tables

--- product_shop/products/models.py
+++ product_shop/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseCategoryModel(models.Model):
+    name = models.CharField(max_length=128, verbose_name='Категория')
+    slug = models.SlugField(unique=True, verbose_name='Идентификатор')
+    image = models.ImageField(upload_to='products/images/')
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseCategoryModel):
+    pass
+
+
+class SubCategory(BaseCategoryModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
